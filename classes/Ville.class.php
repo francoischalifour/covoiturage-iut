ea15b8f7e171b56b7d136cdb21f9ecca5435c5f8
francoischalifour <francois.chalifour@gmail.com>
2014-10-14 18:23:33 +0200

    Ajout de la classe Ville

--- classes/Ville.class.php
+++ classes/Ville.class.php
@@ -1,4 +1,22 @@
 <?php
-class Ville{
-	
+class Ville {
+    private $num;
+    private $nom;
+
+    public function getNum() {
+        return $this->num;
+    }
+
+    public function setNum($num) {
+        $this->num = $num;
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function setNom($nom) {
+        $this->nom = $nom;
+        return $this;
+    }
 }
