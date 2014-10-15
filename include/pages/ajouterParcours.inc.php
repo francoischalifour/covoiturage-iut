52093539c13756d7ab3f363eff6e2e076f9f464f
chovCodeur <michel@lesecuriesdeventadour.com>
2014-10-15 10:33:17 +0200

    Modification fin de TP

--- include/pages/ajouterParcours.inc.php
+++ include/pages/ajouterParcours.inc.php
@@ -1,1 +1,33 @@
 <h1>Ajouter un parcours</h1>
+<?php
+if (!isset($_POST['formParcours'])) {
+	?>
+	<form action ="#" method ="post" name="formParcours">
+		<select class="champ" name="Ville1">
+		
+		</select>
+	</form>
+}
+
+
+
+
+
+
+
+<?php
+if (!isset($_POST['formVille'])) {
+    ?>
+    <form action="#" method="post" name="formVille">
+        <label for="nom">Nom :</label>
+        <input type="text" placeholder="Nom de la ville" class="champ" name="nom">
+        <button type="submit" class="bouton">Valider</button>
+    </form>
+    <?php
+}
+else {
+    // traitement
+    ?>
+    <p>La ville <?php echo $_POST['nom'] ?> a bien été ajoutée</p>
+    <?php
+}
