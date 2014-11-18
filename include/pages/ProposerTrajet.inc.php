a475b258033d6b989fc3510f47a68e7d7f481f83
chovCodeur <michel@lesecuriesdeventadour.com>
2014-11-18 17:31:54 +0100

    Fin de séance, ajout de proposer trajet .. il va falloir bosser à la maison .. c'est vraiment pas cool ça. Vivement jeudi soir

--- include/pages/ProposerTrajet.inc.php
+++ include/pages/ProposerTrajet.inc.php
@@ -1,1 +1,27 @@
 <h1>Proposer un trajet</h1>
+<?php
+require_once("include/autoload.inc.php");
+
+$pdo = new MyPdo();
+$parcoursManager = new ParcoursManager($pdo);
+$villeManager = new VilleManager($pdo);
+
+if (empty($_POST['vil_num'])) {
+$villes = $villeManager->getAllVille();
+?>
+<form action ="#" method ="post">
+    <label for="vil_num">Ville de départ :</label>
+    <select class="form-control" name="vil_num" id="vil_num">
+        <option value="">Sélectionnez la ville</option>
+        <?php
+            foreach ($villes as $ville) {
+            ?>
+            <option value="<?php echo $ville->getVilNum() ?>"><?php echo $ville->getVilNom() ?></option>
+            <?php
+            }
+        ?>
+    </select>
+
+    <button type="submit" class="btn btn-primary">Valider</button>
+</form>
+<?php }
