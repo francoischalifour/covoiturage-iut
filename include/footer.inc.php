7b1c3e04968ed0d5fd87abe3d90475784e325201
francoischalifour <francois.chalifour@gmail.com>
2014-12-04 13:14:53 +0100

    Ajout d'un captcha friendly (amélioration)

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -55,7 +55,9 @@
         $("#pressHuman").on('mouseup', function(e) {
             if (new Date().getTime() >= (start + longpress)) {
                 $('#login').removeAttr('disabled');
-                $('#pressHuman').removeClass('mdi-action-accessibility').removeClass('btn-primary').addClass('mdi-action-done').addClass('btn-success');
+                $('#pressHuman').removeClass('mdi-action-accessibility').removeClass('btn-primary').removeClass('btn-warning').addClass('mdi-action-done').addClass('btn-success');
+            } else {
+                $('#pressHuman').removeClass('btn-primary').addClass('btn-warning');
             }
         });
     });
